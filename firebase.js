23d9f98495a459fc17b9a2ab94f3f66d27b59eca
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2024-12-15 21:21:37 +0530

    firebase security measures

--- firebase.js
+++ firebase.js
@@ -10,13 +10,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-t3hCbzoLuGP2JqUc75qCiyTNDRBXp8M",
-  authDomain: "e-kicker.firebaseapp.com",
-  projectId: "e-kicker",
-  storageBucket: "e-kicker.firebasestorage.app",
-  messagingSenderId: "286842017972",
-  appId: "1:286842017972:web:cfd41a0363cae2f6d69fe1",
-  measurementId: "G-TJ8RS5CYLQ"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:import.meta.env.VITE_FIREBASE_APP_ID, 
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
